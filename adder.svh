b413e942f5b083575eb95613a5606120e28c9fcd
sarahbrande <48233952+sarahbrande@users.noreply.github.com>
2019-03-13 21:20:09 -0600

    Add files via upload

---
+++ adder.svh
@@ -0,0 +1,12 @@
+module adder(a,b,c_in,sum,c_out);
+  input logic [7:0] a;
+  input logic [7:0] b;
+  input logic c_in;
+  output logic [7:0] sum;
+
+  logic [8:0] result;
+
+  assign result = a+b+c_in;
+  assign sum = result[7:0];
+  assign c_out = result[8]
+endmodule: adder
